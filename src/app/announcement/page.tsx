e2a31e87323440c69cbfeda1369d81dac1352a17
pwkang <porweikang@hotmail.com>
2024-02-08 22:28:47 +0800

    feat: add announcement page

---
+++ src/app/announcement/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '../../components/layout/layout';
+import HeroSlider from '../../components/layout/HeroSlider';
+import AnnouncementListing from '../../components/templates/announcement/AnnouncementListing';
+import { defaultMetadata } from '@lib/default-metadata';
+import { Metadata } from 'next';
+
+function Page() {
+  return (
+    <Layout>
+      <HeroSlider />
+      <AnnouncementListing />
+    </Layout>
+  );
+}
+
+export const metadata: Metadata = {
+  ...defaultMetadata,
+  title: '云端布告板 | 马来西亚傅氏总会',
+};
+
+export default Page;
---
+++ src/components/templates/announcement/AnnouncementListing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AnnouncementListing() {
+  return (
+    <div className="max-w-[90%] w-5xl m-auto mt-8">
+      <h1>云端布告板</h1>
+      <div className="flex justify-center py-8 text-xl font-cn">暂无通告</div>
+    </div>
+  );
+}
+
+export default AnnouncementListing;
