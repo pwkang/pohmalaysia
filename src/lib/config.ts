d00b27139af5f63a8357e7b3a0ce08fe18869160
pwkang <porweikang@hotmail.com>
2024-09-24 19:10:21 +0800

    refactor: update fb url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 const config = {
   websiteUrl: 'https://pohmalaysia.com',
-  facebookUrl: 'https://www.facebook.com/malaysia.poh',
+  facebookUrl: 'https://www.facebook.com/profile.php?id=100064815213480',
 };
 
 export default config;
