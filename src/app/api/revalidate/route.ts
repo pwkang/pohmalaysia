34488cdd214f7a7fcfeb7b4aa6cd81b311262dca
Pwkang <porweikang@hotmail.com>
2025-03-13 21:25:20 +0800

    feat: add revalidation (#3)

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,34 @@
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const headers = request.headers;
+
+  const authorization = headers.get('authorization');
+
+  if (authorization !== process.env.STRAPI_WEBHOOK_SECRET) {
+    return NextResponse.json({ revalidated: false, now: Date.now() });
+  }
+
+  if (data.event !== 'entry.publish')
+    return NextResponse.json({ revalidated: false, now: Date.now() });
+
+  const paths = [];
+
+  switch (data.model) {
+    case 'gallery':
+      paths.push(`/gallery/${data.entry.slug}`);
+      break;
+    case 'bod-page':
+      paths.push(`/committee/${data.entry.slug}`);
+      break;
+  }
+
+  for (const path of paths) {
+    await revalidatePath(path);
+    console.log(`Revalidated ${path}`);
+  }
+
+  return NextResponse.json({ revalidated: true, now: Date.now() });
+}
