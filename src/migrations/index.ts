48ae594154aab10d67c0c3ac6d8d7c17cb9de5cb
pwkang <porweikang@hotmail.com>
2025-06-01 02:42:45 +0800

    chore: update build script

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250525_042357 from './20250525_042357';
-
-export const migrations = [
-  {
-    up: migration_20250525_042357.up,
-    down: migration_20250525_042357.down,
-    name: '20250525_042357'
-  },
-];
