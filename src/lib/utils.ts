89ddea086caf366c5d362dfdc6f31509b91a8ec7
pwkang <porweikang@hotmail.com>
2025-05-31 19:31:26 +0800

    fix: environment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
 import classNames, { Argument } from 'classnames';
 import { twMerge } from 'tw-merge';
 
-export const __DEV__ = process.env.ENVIRONMENT === 'development';
+export const __DEV__ = process.env.NEXT_PUBLIC_VERCEL_ENV === 'development';
 
-export const __PROD__ = process.env.ENVIRONMENT === 'production';
+export const __PROD__ = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
 
-export const __PREVIEW__ = process.env.ENVIRONMENT === 'preview';
+export const __PREVIEW__ = process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview';
 
 export const cn = (...classes: Argument[]) => {
   return twMerge(classNames(...classes));
